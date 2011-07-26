907b51733b4f5a0a524bc22b554abf2fd97de402
Dmitry Vyal <akamaus@gmail.com>
2011-07-26 22:37:02 +0400

    refactored to use prototypes, creating neurons and links, dragging neurons and redrawing links

--- spike.js
+++ spike.js
@@ -1,16 +1,21 @@
 neuron_radius = 20;
 
-function Spike() {
-function Neuron(paper, x, y) {
-    var soma = paper.circle(x,y, neuron_radius);
-    soma.attr({fill: '#fff'});
+function Pos(x,y) {
+    this.x = x;
+    this.y = y;
+}
+Pos.prototype.toString = function() {
+    return this.x + " " + this.y;
+}
 
-    soma.getPos = function() {
-        return {x: this.attr("cx"), y: this.attr("cy")};
-    }
-    soma.setPos = function(pos) {
-        this.attr({cx: pos.x, cy: pos.y});
-    }
+function Neuron(x, y) {
+    this.num = Neuron.new_num++;
+    this.soma = this.paper.circle(x,y, neuron_radius);
+    this.soma.neuron = this;
+    this.outgoing_links = [];
+    this.incoming_links = [];
+
+    this.soma.attr({fill: '#fff'});
 
     var on_drag_start = function() {
         this.opos = this.getPos();
@@ -23,23 +28,48 @@ function Neuron(paper, x, y) {
 
     var on_drag_move = function(dx, dy) {
         this.setPos({x: this.opos.x + dx, y: this.opos.y + dy});
+        $(this.neuron.outgoing_links).each(function(k,v) {v.redraw() });
+        $(this.neuron.incoming_links).each(function(k,v) {v.redraw() });
     }
-    soma.drag(on_drag_move, on_drag_start, on_drag_stop);
+    this.soma.drag(on_drag_move, on_drag_start, on_drag_stop);
 
-    soma.click(function() {
-            if (Neuron.selected) {
-                alert(Neuron.selected);
+    this.soma.click(function() {
+            var n1 = Neuron.selected,
+                n2 = this.neuron;
+
+            if (n1 && n1 != n2 && !Neuron.linked(n1,n2)) {
+                new Link(n1,n2);
                 Neuron.selected = undefined;
             } else {
-                Neuron.selected = this;
+                Neuron.selected = this.neuron;
             }
         });
+
+    this.neurons.push(this);
 }
 
-function Link(paper, n1, n2) {
-    var axon = paper.path("M");
+Neuron.prototype.toString = function() {return "N " + this.num; }
+Neuron.prototype.getPos = function() { return this.soma.getPos() };
+Neuron.prototype.setPos = function(p) { this.soma.setPos(p) };
+Neuron.linked = function(n1,n2) {
+        for (i in n1.outgoing_links) {
+        if (n1.outgoing_links[i].n2 === n2) return true;
+    }
+    return false;
 }
 
+function Link(n1, n2) {
+    this.n1 = n1;
+    this.n2 = n2;
+    n1.outgoing_links.push(this);
+    n2.incoming_links.push(this);
+    this.axon = this.paper.path("M" + n1.getPos() + "L" + n2.getPos());
+}
+
+Link.prototype.toString = function() { return this.n1 + " -> " + this.n2 }
+Link.prototype.redraw = function() {this.axon.attr({path: "M" + this.n1.getPos() + "L" + this.n2.getPos()})};
+
+function Spike() {
     var on_canvas_click = function(e) {
         var x = e.pageX - this.offsetLeft;
         var y = e.pageY - this.offsetTop;
@@ -47,12 +77,26 @@ function Link(paper, n1, n2) {
         $("#status-bar").html("x = " + x + "; y = " + y);
 
         if(e.originalEvent.explicitOriginalTarget.tagName == "svg")
-            neurons.push(new Neuron(paper, x, y));
+            new Neuron(x, y);
     }
 
     var neurons = [];
     var links = [];
     var paper = Raphael("main-bar", "100%", "100%");
+    Neuron.prototype.paper = paper;
+    Neuron.prototype.neurons = neurons;
+    Neuron.new_num = 1;
+    Link.prototype.paper = paper;
+    Link.prototype.links = links;
+
+    c = paper.circle();
+    c.__proto__.getPos = function() {
+        return new Pos(this.attr("cx"), this.attr("cy"));
+    }
+    c.__proto__.setPos = function(pos) {
+        this.attr({cx: pos.x, cy: pos.y});
+    }
+    c.remove();
 
     // binding handlers
     $("#main-bar").click(on_canvas_click);
